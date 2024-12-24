24df510fe3734b405aae98bbe7cb0af329cbaffe
Sujoy Paul <freelancersujoy@gmail.com>
2024-12-24 13:54:20 +0600

    dynamic border on scroll

--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -1,12 +1,32 @@
+'use client'
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 const logo = '/images/logo-black.png'
 import { IoSearch } from "react-icons/io5";
 import { CgMenuRight } from "react-icons/cg";
 
 const Navbar = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="fixed w-full z-50">
+    <div className={`fixed w-full z-50 ${isScrolled && 'border-gray-200 border-b-[1px]'}`}>
       <nav className="bg-[#FFFFFF]">
         <div className="md:mx-[50px] mx-[20px] py-[28px] flex justify-between items-center">
           <div>
