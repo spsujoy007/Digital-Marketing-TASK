b38dfd46fa429040e7c2872b441c0ae450071137
Sujoy Paul <freelancersujoy@gmail.com>
2024-12-25 11:40:54 +0600

    ta

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
